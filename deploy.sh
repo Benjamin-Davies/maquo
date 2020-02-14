6fadd524823e6abd4b970a73099e53151c24b364
Benjamin-Davies <bentendavies@gmail.com>
2020-02-14 18:27:35 +1300

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo 'Connecting to the server'
+
+SFTP_ARGS="-P 2222 benjamindavies@php.mmc.school.nz"
+
+if command -v sshpass >/dev/null \
+  && [ -n "$SSHPASS" ]
+then
+  sshpass -e sftp $SFTP_ARGS
+else
+  sftp $SFTP_ARGS
+fi <<EOF
+cd Web/
+ls
+cd 201COS/benjamindavies/maquo/
+mkdir src
+put -r src
+put index.html
+EOF
