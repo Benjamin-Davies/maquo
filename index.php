a5b9553384996f0629ef3a0e227556c76344302d
Benjamin Davies <bentendavies@gmail.com>
2020-03-02 13:25:47 +1300

    Change styles (not josh's style)

--- index.php
+++ index.php
@@ -12,21 +12,13 @@ $page_title = 'Welcome';
 </header>
 
 <main class="MainColumn">
+<?php for ($i = 0; $i < 5; $i++) { ?>
     <section class="Card">
-        <h1>A Quizz</h1>
+        <h1>A<?php for ($j = 0; $j < $i; $j++) { echo 'nother'; } ?> Quizz</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ut?</p>
         <p class="Card__action"><a href="quiz#123456" class="button button--secondary">Take The Quiz</a></p>
     </section>
-    <section class="Card">
-        <h1>Another Quizz</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ut?</p>
-        <p class="Card__action"><a href="quiz#123456" class="button button--secondary">Take The Quiz</a></p>
-    </section>
-    <section class="Card">
-        <h1>Yet Another Quizz</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ut?</p>
-        <p class="Card__action"><a href="quiz#123456" class="button button--secondary">Take The Quiz</a></p>
-    </section>
+<?php } ?>
 </main>
 
 <?php include 'util/components/end.php'; ?>
