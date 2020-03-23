bdf8ac1da2d0657512515acaefed0baf7cc02d0e
Benjamin-Davies <bentendavies@gmail.com>
2020-03-23 18:01:04 +1300

    Docker works with mysql

--- util/db/connect.php
+++ util/db/connect.php
@@ -1,4 +1,5 @@
 <?php
+$db_host = 'mysql';
 $db_name = 'benjamindavies_maquo';
 $db_username = 'benjamindavies';
 $db_password = 'secret';
@@ -10,12 +11,13 @@ if (file_exists($secrets_file)) {
 
 try {
     $db = new PDO(
-        "mysql:host=localhost;" .
+        "mysql:host=$db_host;" .
         "dbname=$db_name",
         $db_username, $db_password);
 } catch (PDOException $e) {
     http_response_code(500);
     header('Content-Type: text/plain');
     echo "Failed to connect to db\n";
+    echo $e;
     exit();
 }
