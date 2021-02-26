e8f227a626192032299e9ccab7f89e4091aa972b
Benjamin-Davies <bentendavies@gmail.com>
2021-02-26 15:05:06 +1300

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+require 'util/db/quizzes.php';
+require 'util/db/users.php';
+
+$root_url = '.';
+$page_title = '404';
+?>
+<?php include 'util/components/begin.php'; ?>
+
+<header class="Header MainColumn">
+    <h1>Oops!</h1>
+    <p class="blurb">Sorry, but that page does not exist...</p>
+    <p>
+        <a href="/" class="button">Return to Home</a>
+    </p>
+</header>
+
+<?php include 'util/components/end.php'; ?>
