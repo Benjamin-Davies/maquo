4ba660d48a9fb650b6616191dea994f4ab29b0e0
Benjamin-Davies <bentendavies@gmail.com>
2021-02-26 14:20:52 +1300

    Add loading client_id from env

--- util/oauth.php
+++ util/oauth.php
@@ -1,2 +1,12 @@
 <?php
-$oauth = json_decode(file_get_contents(__DIR__.'/../client_secret.json'));
+
+$oauth_secret_file = __DIR__.'/../client_secret.json';
+if (file_exists($oauth_secret_file)) {
+  $oauth = json_decode(file_get_contents($oauth_secret_file));
+} elseif (getenv('OAUTH_CLIENT_ID')) {
+  $oauth = (object) [
+    'web' => (object) [
+      'client_id' => getenv('OAUTH_CLIENT_ID'),
+    ],
+  ];
+}
